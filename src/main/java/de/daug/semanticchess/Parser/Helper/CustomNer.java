6b99101461b939386b5a73f70f57cfce0ab31caa
Jörn <jhdaug@googlemail.com>
2017-09-30 16:50:43 +0200

    Upd: added opening check
    - finds the opening from the query in the database and returns the correct name

--- src/main/java/de/daug/semanticchess/Parser/Helper/CustomNer.java
+++ src/main/java/de/daug/semanticchess/Parser/Helper/CustomNer.java
@@ -86,4 +86,33 @@ public class CustomNer {
 		
 		return tokens;
 	}
+	
+	
+	public List<Token> checkOpening(List<Token> tokens){
+		for(int i = 0; i < tokens.size(); i++){
+			String word = tokens.get(i).getWord().toLowerCase();
+			String value = vocabulary.INVERSED_PROPERTIES.get(word);
+			if(word.equals("game") && tokens.get(i).getPos().equals("NNP")){
+				value = "OPENING";
+			}
+			if (value != null && value.equals("OPENING")) {
+				tokens.get(i).setNe(value);
+				int j = i-1;
+				while(tokens.get(j) != null && (!tokens.get(j).getNe().equals("O") || tokens.get(j).getPos().equals("POS") || tokens.get(j).getPos().equals("NNP"))){
+					tokens.get(j).setNe(value);
+					j--;
+				}
+				int k = i+1;
+				while(tokens.get(k) != null && (!tokens.get(k).getNe().equals("O") || tokens.get(k).getPos().equals("POS") || tokens.get(k).getPos().equals("NNP"))){
+					tokens.get(k).setNe(value);
+					k++;
+				}
+				
+			}
+			
+		}
+		
+		
+		return tokens;
+	}
 }
