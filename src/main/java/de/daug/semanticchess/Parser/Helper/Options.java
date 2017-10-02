8cf8d22afe64f2ab080ffd70015e10018212fb9e
Jörn <jhdaug@googlemail.com>
2017-10-02 10:31:51 +0200

    Upd: added group by

--- src/main/java/de/daug/semanticchess/Parser/Helper/Options.java
+++ src/main/java/de/daug/semanticchess/Parser/Helper/Options.java
@@ -3,44 +3,48 @@ package de.daug.semanticchess.Parser.Helper;
 /**
  * options for the sparql query
  */
-public class Options{
-	
+public class Options {
+
 	private static final String LIMIT = "LIMIT ";
 	private static final String OFFSET = "OFFSET ";
 	private static final String ORDERBY = "ORDER BY ";
 	private static final String GROUPBY = "GROUP BY ";
 	private static final String HAVING = "HAVING ";
-	
-	private String limitStr;
-	private String offsetStr;
+
+	private String limitStr ="";
+	private String offsetStr = "";
 	private String orderStr = "";
-	private String groupStr;
-	private String havingStr;
-	
+	private String groupStr = "";
+	private String havingStr ="";
+
 	/**
 	 * constructor
 	 */
-	public Options(){
+	public Options() {
 	}
 
 	/**
 	 * get limit
+	 * 
 	 * @return limitStr
 	 */
 	public String getLimitStr() {
 		return limitStr;
 	}
-	
+
 	/**
 	 * set limit string
-	 * @param limit: integer
+	 * 
+	 * @param limit:
+	 *            integer
 	 */
 	public void setLimitStr(int limit) {
 		this.limitStr = LIMIT + limit;
 	}
-	
+
 	/**
 	 * get offset
+	 * 
 	 * @return offsetStr
 	 */
 	public String getOffsetStr() {
@@ -49,7 +53,9 @@ public class Options{
 
 	/**
 	 * set offset string
-	 * @param offset: integer
+	 * 
+	 * @param offset:
+	 *            integer
 	 */
 	public void setOffsetStr(int offset) {
 		this.offsetStr = OFFSET + offset;
@@ -57,47 +63,65 @@ public class Options{
 
 	/**
 	 * get order string
+	 * 
 	 * @return
 	 */
 	public String getOrderStr() {
 		return orderStr;
 	}
-	
+
 	/**
 	 * set order String
-	 * @param orderDirection: ASC or DESC
-	 * @param order: class that should be ordered
+	 * 
+	 * @param orderDirection:
+	 *            ASC or DESC
+	 * @param order:
+	 *            class that should be ordered
 	 */
 	public void setOrderStr(String orderDirection, String order) {
-		if(orderStr.equals("")){
+		if (orderStr.equals("")) {
 			this.orderStr = ORDERBY + orderDirection + "(" + order + ") ";
 		} else {
-			this.orderStr += orderDirection +  "(" + order + ") "; 
+			this.orderStr += orderDirection + "(" + order + ") ";
 		}
 	}
-	
+
+	public String getGroupStr() {
+		return groupStr;
+	}
+
+	public void setGroupStr(String group) {
+		if (groupStr.equals("")) {
+			this.groupStr = GROUPBY + group;
+		} else {
+			this.orderStr += group;
+		}
+	}
+
 	/**
 	 * toString method
 	 */
 	@Override
 	public String toString() {
-		
-		if(limitStr == null && offsetStr == null && orderStr == null){
-			return ""; 
-		}
-		else if(limitStr != null && offsetStr != null && orderStr == null){
+
+		if (limitStr == null && offsetStr == null && orderStr == null && groupStr == null) {
+			return "";
+		} else if (limitStr != null && offsetStr != null && orderStr == null && groupStr == null) {
 			return limitStr + " " + offsetStr;
-		}else if (limitStr != null && offsetStr != null && orderStr != null){
+		} else if (limitStr != null && offsetStr != null && orderStr != null && groupStr == null) {
 			return orderStr + " " + limitStr + " " + offsetStr;
-		} else if (limitStr == null && offsetStr == null && orderStr != null){
+		} else if (limitStr != null && offsetStr != null && orderStr != null && groupStr != null) {
+			return groupStr+ " " +orderStr + " " + limitStr + " " + offsetStr;
+		} else if (limitStr == null && offsetStr == null && orderStr != null && groupStr == null) {
 			return orderStr;
+		} else if (limitStr == null && offsetStr == null && orderStr != null && groupStr != null) {
+			return groupStr + " " + orderStr;
 		}
-				
+		else if (limitStr == null && offsetStr == null && orderStr == null && groupStr != null) {
+			return groupStr;
+		}
+
 		return limitStr + " " + offsetStr;
-	}	
-	
-	
-	
-	
-	
+	}
+
 }
