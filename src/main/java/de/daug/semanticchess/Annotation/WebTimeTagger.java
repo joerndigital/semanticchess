8c5b62923cddc078f3eaad23d40624c5f0d0d211
Jörn <jhdaug@googlemail.com>
2017-10-31 16:57:50 +0100

    Add: prepared for later work

---
+++ src/main/java/de/daug/semanticchess/Annotation/WebTimeTagger.java
@@ -0,0 +1,24 @@
+package de.daug.semanticchess.Annotation;
+
+//import java.text.DateFormat;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import java.util.List;
+//import java.util.Properties;
+//
+//import de.daug.semanticchess.Configurations;
+//import edu.stanford.nlp.ling.CoreAnnotations;
+////import edu.stanford.nlp.ling.CoreLabel;
+//import edu.stanford.nlp.pipeline.*;
+//import edu.stanford.nlp.time.*;
+//import edu.stanford.nlp.util.CoreMap;
+
+
+/**
+ * This class uses https://nlp.stanford.edu/software/sutime.html
+ * It analyzes dates in the query and returns date string (yyyy-mm-dd).
+ */
+public class WebTimeTagger {
+
+//kein sutime unter Configurations.WEB_SERVER
+}
